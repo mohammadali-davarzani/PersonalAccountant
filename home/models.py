64b06a09e1e645d14a2699b4fdd558a738e0ec57
Mohammadali Davarzani <moa.davarzani@gmail.com>
2021-12-07 13:37:34 +0330

    create expense model and add to admin

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Expense(models.Model):
+    
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    description = models.TextField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+
+    class Meta:
+        verbose_name = "Expense"
+        verbose_name_plural = "Expenses"
+
+    def __str__(self):
+        return "{} | {}".format(self.title, self.user)
